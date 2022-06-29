ab8c452ffa5ac5dae06aa3d0abbc53391ac85d88
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-06-29 16:09:25 +0900

    Use Presenter in LikesVC

--- CleanGithub-practice/Framework_Driver/UI/LikesViewController.swift
+++ CleanGithub-practice/Framework_Driver/UI/LikesViewController.swift
@@ -11,6 +11,7 @@ final class LikesViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     
     private var viewDataArray = [GitHubRepoViewData]()
+    private weak var presenter: ReposPresenterProtocol!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +19,32 @@ final class LikesViewController: UIViewController {
         setup()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        // SearchVCからtabで遷移後、再度LikedReposを読み込む（SearchVCでLikeが更新される可能性があるため）
+        presenter.collectLikedRepos()
+    }
+    
     private func setup() {
         tableView.estimatedRowHeight = 64
         tableView.rowHeight = UITableView.automaticDimension
         tableView.register(LikeRepositoryCell.nib(), forCellReuseIdentifier: LikeRepositoryCell.identifier)
+    }
+}
+
+// MARK: - ReposPresenterInjectable
+extension LikesViewController: ReposPresenterInjectable {
+    func inject(reposPresenter: ReposPresenterProtocol) {
+        presenter = reposPresenter
+        presenter.likesOutput = self
+        presenter.collectLikedRepos()
+    }
+}
+
+// MARK: - LikesPresenterOutput
+extension LikesViewController: LikesPresenterOutput {
+    func update(by viewDataArray: [GitHubRepoViewData]) {
+        self.viewDataArray = viewDataArray
+        tableView.reloadData()
     }
 }
 
@@ -41,5 +64,10 @@ extension LikesViewController: UITableViewDataSource {
 
 // MARK: - UITableViewDelegate
 extension LikesViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let viewData = viewDataArray[indexPath.row]
+        // お気に入り状態をToggle
+        presenter.set(liked: !viewData.isLiked, for: viewData.id)
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
 }
